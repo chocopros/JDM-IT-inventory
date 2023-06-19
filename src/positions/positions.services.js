34ab031c26677f69b80fd70238a556bcb21b8b1a
Jesuschock93 <jesuschock93@gmail.com>
2023-06-19 18:20:54 -0400

    save

--- src/positions/positions.services.js
+++ src/positions/positions.services.js
@@ -6,7 +6,23 @@ const positionsControllers = require('./positions.controllers');
 const createNewPosition = ( req ,res ) => {
     const {position,description} = req.body;
 
-    positionsControllers.createNewPosition({position,description})
-        .then()
-        .catch()
+    if (position && description) {
+        positionsControllers.createNewPosition({position,description})
+        .then(r => res.status(200).json({message: 'Position Created'}))
+        .catch(err => res.status(400).json({message: err}))
+    } else {
+        res.status(400).json({
+            messageError: 'Fails Fields Register',
+            fields: {
+                "position": "name of position",
+                "description": "description of position"
+            }
+        })
+    }
+
+    
 };
+
+module.exports = {
+    createNewPosition
+}
