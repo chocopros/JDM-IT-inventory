64a164d9197ea8fc915ec93b614220a45bc6bb6f
Jesuschock93 <jesuschock93@gmail.com>
2023-06-23 21:57:35 -0400

    requestControllers

---
+++ src/request/request.controllers.js
@@ -0,0 +1,37 @@
+//?DEPENDENCIES
+const Request = require('../models/request.models');
+const RequestComputer = require('../models/requestComputer.models');
+
+
+
+// CREATE NEW REQUEST
+const createNewRequest = async (dataRequest) => {
+    return await Request.create({
+        typesSupportId: dataRequest.typesSupportId,
+        referenceId: dataRequest.referenceId,
+        description: dataRequest.description,
+        statusId: dataRequest.statusId
+    })
+};
+
+// GET ALL REQUEST
+const getAllRequest = async () => {
+    return await Request.findAll()
+};
+
+
+//! **** >>>ASSINAMENT<<< ****
+
+//> CREATE ASSIGNMENT
+const createNewAssignment = async (dataAssigment) => {
+    return await RequestComputer.create({
+        requestId: dataAssigment.requestId,
+        computerId: dataAssigment.computerId
+    })
+};
+
+module.exports = {
+    createNewRequest,
+    createNewAssignment,
+    getAllRequest
+}
